23fd8c3667ffc157a971c58e6050a7530617a997
Charan6924 <cxv166@case.edu>
2026-02-22 17:00:32 -0500

    going back to old aarchitecture

--- Generator.py
+++ Generator.py
@@ -1,97 +1,94 @@
 import torch
 import torch.nn as nn
 
-class Generator(nn.Module):
-    def __init__(self, in_channels=3, out_channels=3, num_features=64, growth_channels=32, num_blocks=8, num_class=2, residual_scale=0.2):
-        super(Generator, self).__init__()
-        self.residual_scale = residual_scale
-        self.head = nn.Conv2d(in_channels, num_features, kernel_size=3, padding=1)
-        self.body = nn.Sequential(*[RRDB(num_features, growth_channels, residual_scale) for _ in range(num_blocks)])
-        self.body_tail = nn.Conv2d(num_features, num_features, kernel_size=3, padding=1)
-        self.upsample = nn.Sequential(
-            nn.Conv2d(num_features, num_features * 4, kernel_size=3, padding=1),
-            nn.PixelShuffle(2), 
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        self.upsample_refine = nn.Sequential(
-            nn.Conv2d(num_features, num_features, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(num_features, num_features, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        self.tail = nn.Sequential(
-            nn.Conv2d(num_features, num_features // 2, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(num_features // 2, out_channels, kernel_size=3, padding=1),
-            # No sigmoid - clamp in forward() to avoid gradient saturation
-        )
-        self._init_weights()
 
-    def _init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, a=0.2, mode='fan_in', nonlinearity='leaky_relu')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-
-        # Initialize final conv with small weights to start near 0
-        final_conv = self.tail[-1]  # Last layer is now the conv
-        nn.init.normal_(final_conv.weight, mean=0, std=0.01)
-        nn.init.zeros_(final_conv.bias)
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2):
+        super(UpsampleBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels * (scale_factor ** 2),
+                              kernel_size=3, padding=1)
+        self.pixel_shuffle = nn.PixelShuffle(scale_factor)
+        self.prelu = nn.PReLU()
 
     def forward(self, x):
-        x = self.head(x)
-        feat = x
-        x = self.body(x)
-        x = self.body_tail(x) + feat
-        x = self.upsample(x)
-        x = self.upsample_refine(x)
-        x = self.tail(x)
-        x = torch.clamp(x, 0, 1)  # Clamp instead of sigmoid - gradients flow freely
-        x = x[:, :, :125, :125]
+        x = self.conv(x)
+        x = self.pixel_shuffle(x)
+        x = self.prelu(x)
         return x
 
 
-class RRDB(nn.Module):
-    def __init__(self, num_features=64, growth_channels=32, residual_scale=0.2):
-        super().__init__()
-        self.residual_scale = residual_scale
-        self.rdb1 = ResidualBlock(num_features, growth_channels, residual_scale)
-        self.rdb2 = ResidualBlock(num_features, growth_channels, residual_scale)
-        self.rdb3 = ResidualBlock(num_features, growth_channels, residual_scale)
+class ChannelAttention(nn.Module):
+    def __init__(self, channels, reduction=8):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(channels, channels // reduction, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels // reduction, channels, kernel_size=1),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        out = self.rdb1(x)
-        out = self.rdb2(out)
-        out = self.rdb3(out)
-        return x + self.residual_scale * out
+        attention = self.avg_pool(x)
+        attention = self.fc(attention)
+        return x * attention
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, num_features=64, growth_channels=32, residual_scale=0.2):
-        super().__init__()
-        self.residual_scale = residual_scale
-        self.layers = nn.ModuleList()
-        for i in range(5):
-            in_ch = num_features + i * growth_channels
-            self.layers.append(DenseLayer(in_ch, growth_channels))
-        fusion_in = num_features + 5 * growth_channels
-        self.fusion = nn.Conv2d(fusion_in, num_features, kernel_size=1)
+class ResBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.prelu = nn.PReLU(num_parameters=channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
 
     def forward(self, x):
-        features = [x]
-        for layer in self.layers:
-            out = layer(torch.cat(features, dim=1))
-            features.append(out)
-        fused = self.fusion(torch.cat(features, dim=1))
-        return x + self.residual_scale * fused
+        residual = self.conv1(x)
+        residual = self.bn1(residual)
+        residual = self.prelu(residual)
+        residual = self.conv2(residual)
+        residual = self.bn2(residual)
+        return x + 0.1 * residual
 
 
-class DenseLayer(nn.Module):
-    def __init__(self, in_channels, growth_channels=32):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, growth_channels, kernel_size=3, padding=1)
-        self.act  = nn.LeakyReLU(0.2, inplace=True)
+class Generator(nn.Module):
+    def __init__(self, resblocks=16, channels=3, base_channels=64, use_attention=True):
+        super(Generator, self).__init__()
+        self.conv_input = nn.Sequential(
+            nn.Conv2d(channels, base_channels, kernel_size=9, padding=4),
+            nn.PReLU()
+        )
+
+        res_blocks = []
+        for i in range(resblocks):
+            res_blocks.append(ResBlock(base_channels))
+            if use_attention and i in {7, 15}:  # only 2 attention blocks
+                res_blocks.append(ChannelAttention(base_channels))
+
+        self.resblocks = nn.Sequential(*res_blocks)
+
+        self.conv_mid = nn.Sequential(
+            nn.Conv2d(base_channels, base_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_channels)
+        )
+
+        self.upsample1 = UpsampleBlock(base_channels, base_channels)  # 64→128
+
+        self.conv_output = nn.Sequential(
+            nn.Conv2d(base_channels, base_channels, kernel_size=3, padding=1),
+            nn.PReLU(),
+            nn.Conv2d(base_channels, channels, kernel_size=9, padding=4)
+        )
+
+        self.final_resize = nn.Upsample(size=(125, 125), mode='bilinear', align_corners=False)
 
     def forward(self, x):
-        return self.act(self.conv(x))
+        x1 = self.conv_input(x)
+        x2 = self.resblocks(x1)
+        x3 = self.conv_mid(x2)
+        x_mid = x1 + x3
+        x_up = self.upsample1(x_mid)
+        out = self.conv_output(x_up)
+        out = self.final_resize(out)
+        return out
